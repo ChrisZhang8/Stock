163238e8a5101351fd8e20fb621c907b205c572a
chris <hjjzhangkui@163.com>
2017-04-27 16:56:37 +0800

    将js从jsp中分离出来

--- src/main/webapp/com/zxj/main/addSaleOrder.js
+++ src/main/webapp/com/zxj/main/addSaleOrder.js
@@ -1,6 +1,3 @@
-/**
- * Created by chris on 2017/4/13.
- */
 
 function insert(){
     var row = $('#detail').datagrid('getSelected');
@@ -13,7 +10,7 @@ function insert(){
 
     $('#detail').datagrid('insertRow', {
         index: index,
-        row:{'PRODUCT':'','BRAND':'','VENDOR':'','SHAPE':'','BATCH_NO':'','ORDER_QTY':0,'RECEIVE_QTY':0,'PRICE':0.00,'TOTAL_AMOUNT':'0.00'}
+        row:{}
     });
     $('#detail').datagrid('selectRow',index);
     $('#detail').datagrid('beginEdit',index);
@@ -42,33 +39,36 @@ function del(){
 function save(){
     var data = $('#detail').datagrid('getData');
     //alert(JSON.stringify(data));
-    //var fmdata = $("#buyorderform").serialize();
+    //var fmdata = $("#saleorderform").serialize();
 
-    $('#buyorderform').form('submit', {
-        url:root+'/com/zxj/dbm/BuyOrderServlet?method=addBuyOrder',
+    $('#saleorderform').form('submit', {
+        url:root+'/com/zxj/dbm/SaleOrderServlet?method=addSaleOrder',
         onSubmit: function(param){
-            if(data.total==0){
+            param.pdata=JSON.stringify(data.rows);
+            if (data.total == 0){
                 $.messager.alert('error','没有订单明细信息！','error');
                 return false;
             }
-            param.pdata=JSON.stringify(data.rows);
-            //alert(JSON.stringify(param));
+            var isValid = $(this).form('validate');
+            if (!isValid){
+                $.messager.progress('close');	// hide progress bar while the form is invalid
+            }
+            return isValid;	// return false will stop the form submission
         },
         success:function(data){
-            var dt = JSON.parse(data);
-            if (dt.status == 'N'){
-                $.messager.alert('error',dt.message);
-            }else {
-                $.messager.alert('info',dt.result);
-
-                $('#buyorderform').form('clear');
-                $('#buyorderform').form('load',{
-                    ORDER_NO:uuid(8, 16)
+            var rs = JSON.parse(data);
+            if(rs.status == 'N'){
+                $.messager.alert('error',rs.message,'error');
+            }else{
+                $.messager.alert('info',rs.result,'info');
+                $('#saleorderform').form('clear');
+                $('#saleorderform').form('load',{
+                    SALE_ORDER_NO:uuid(8, 16)
                 });
                 $('#detail').datagrid('loadData',{"total":0,"rows":[]});
-
             }
 
+            //alert(data)
         }
     });
 
@@ -82,8 +82,9 @@ function reject(){
 
 $(function(){
 
-    $('#buyorderform').form('load',{
-        ORDER_NO:uuid(8, 16)
+
+    $('#saleorderform').form('load',{
+        SALE_ORDER_NO:uuid(8, 16)
     });
 
     $('#detail').datagrid({
@@ -98,28 +99,11 @@ $(function(){
         columns:[[
             //{field:"ck",checkbox:"true"},
             {field:'PRODUCT',title:'产品型号',width:'10%',editor:'text'},
-            {field:'BRAND',title:'品牌',width:'10%',editor:'text'},
-            {field:'BATCH_NO',title:'批号',width:'10%',editor:'text'},
-            {field:'ORDER_QTY',title:'订货量',align:'right',width:'10%',editor:{type:'numberbox'}},
-            {field:'VENDOR',title:'供应商',width:'10%',editor:'text'},
-            {field:'SHAPE',title:'封装',width:'10%',editor:'text'},
-            {field:'RECEIVE_QTY',title:'到货量',align:'right',width:'8%',editor:{type:'numberbox',value:0}},
-            {field:'TAX_POINT',title:'税点',width:'8%',
-                formatter:function(value,row){
-                    return row.IS_TAX_NAME || value;
-                },
-                editor:{
-                    type:'combobox',
-                    options:{
-                        valueField:'TAX_POINT',
-                        textField:'TAX_POINT_NAME',
-                        data:[{"TAX_POINT_NAME":"增值税-17%","TAX_POINT":"17"},
-                            {"TAX_POINT_NAME":"普通税-5%","TAX_POINT":"5"},
-                            {"TAX_POINT_NAME":"不含税","TAX_POINT":"0"}],
-                        required:true
-                    }
-                }
-            },
+            {field:'PRODUCT_NAME',title:'名称',width:'5%',editor:'text'},
+            {field:'BRAND',title:'品牌',width:'5%',editor:'text'},
+            {field:'PACKAGE',title:'封装',width:'5%',editor:'text'},
+            {field:'BATCH_NO',title:'批号',width:'5%',editor:'text'},
+            {field:'ORDER_QTY',title:'订单量',align:'right',width:'10%',editor:{type:'numberbox'}},
             {field:'PRICE',title:'单价',align:'right',width:'5%',editor:{type:'numberbox',options:{precision:2}}},
             {field:'TOTAL_AMOUNT',title:'总金额',align:'right',width:'5%',editor:{type:'numberbox',options:{precision:2}}},
 
@@ -136,6 +120,7 @@ $(function(){
         },
 
         onEndEdit:function(index,row){
+
             row.editing = false;
             $(this).datagrid('refreshRow', index);
         },
@@ -145,9 +130,9 @@ $(function(){
         },
         onBeginEdit:function(rowIndex){
             var editors = $(this).datagrid('getEditors', rowIndex);
-            var n1 = $(editors[3].target);
-            var n2 = $(editors[8].target);
-            var n3 = $(editors[9].target);
+            var n1 = $(editors[5].target);
+            var n2 = $(editors[6].target);
+            var n3 = $(editors[7].target);
             n1.add(n2).numberbox({
                 onChange:function(){
                     var cost = n1.numberbox('getValue')*n2.numberbox('getValue');
@@ -168,39 +153,24 @@ $(function(){
 
     $('#fileupload').fileupload({
         dataType: 'json',
-        url:root+'/com/zxj/dbm/BuyOrderServlet?method=importExcel',
+        url:root+'/com/zxj/dbm/SaleOrderServlet?method=importExcel',
         add: function (e, data) {
             //data.context = $('<p/>').text('Uploading...').appendTo(document.body);
             data.submit();
         },
         done: function (e, data) {
-            //alert(data.result);
-            $('#detail').datagrid('loadData',data.result);
+            if (data.status=='N'){
+                $.messager.alert('error',data.message);
+            }else {
+                //alert(JSON.stringify(data.result));
+                $('#detail').datagrid('loadData',data.result);
+            }
+
         }
     });
 
 
 });
-function getRowIndex(target){
-    var tr = $(target).closest('tr.datagrid-row');
-    return parseInt(tr.attr('datagrid-row-index'));
-}
-function editrow(target){
-    $('#detail').datagrid('beginEdit', getRowIndex(target));
-}
-function deleterow(target){
-    $.messager.confirm('Confirm','Are you sure?',function(r){
-        if (r){
-            $('#detail').datagrid('deleteRow', getRowIndex(target));
-        }
-    });
-}
-function saverow(target){
-    $('#detail').datagrid('endEdit', getRowIndex(target));
-}
-function cancelrow(target){
-    $('#detail').datagrid('cancelEdit', getRowIndex(target));
-}
 
 
 function uuid(len, radix) {
@@ -228,5 +198,6 @@ function uuid(len, radix) {
             }
         }
     }
+
     return uuid.join('');
 }
